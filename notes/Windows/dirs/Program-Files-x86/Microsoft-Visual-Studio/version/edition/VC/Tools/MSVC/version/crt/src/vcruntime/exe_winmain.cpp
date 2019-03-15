cc666f7b9fb5de17c0c1bb2c07d0022c76c0dab0
Rene Nyffenegger <mail@renenyffenegger.ch>
2019-03-15 17:17:01 +0100

    2019-03-15

---
+++ notes/Windows/dirs/Program-Files-x86/Microsoft-Visual-Studio/version/edition/VC/Tools/MSVC/version/crt/src/vcruntime/exe_winmain.cpp
@@ -0,0 +1,8 @@
+$ exe_winmain.cpp
+
+This file contains the `WinMainCRTStartup` entry point.
+
+sa:
+  → Windows/dirs/Program-Files-x86/Microsoft-Visual-Studio/version/edition/VC/Tools/MSVC/version/crt/src/vcruntime/mcrtexe_cpp[`mcrtexe.cpp`]
+
+  → Windows/development/Visual-Studio/link/options/entry
