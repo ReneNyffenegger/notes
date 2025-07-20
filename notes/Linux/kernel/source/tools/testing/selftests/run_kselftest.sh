a633b7f28a3b255fc02dc5e67e424a7618e0554a
René Nyffenegger <mail@renenyffenegger.ch>
2025-07-20 22:52:55 +0200

    2025-07-20

---
+++ notes/Linux/kernel/source/tools/testing/selftests/run_kselftest.sh
@@ -0,0 +1,17 @@
+$ tools/testing/selftests/run_kselftest.sh
+
+`tools/testing/selftests/run_kselftest.sh` runs installed *kselftest* tests.
+
+{ TODO
+
+  In linux kernel tree top level directory:
+code {
+make quicktest=1 run_tests
+make quicktest=1 kselftest
+code }
+
+}
+
+sa:
+
+ `→ Linux/kernel/source/tools/testing/selftests`
